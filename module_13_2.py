2e0d5711b5c14a37e1409d0b5ffa54e1d72161e2
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-10-12 21:37:27 +0700

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -11,12 +11,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands=['start'])
-async def start(messange):
+async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
-async def all_messange(messange):
+async def all_messange(message):
     print('Введите команду /start, чтобы начать общение.')
 
 
