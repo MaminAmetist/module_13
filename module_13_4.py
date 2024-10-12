deab0ec3ae4bac0afc60fff310c374864c1b921f
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-10-13 00:49:04 +0700

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,75 @@
+# Машина состояний
+# Задача "Цепочка вопросов":
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    gender = State()
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.')
+
+
+@dp.message_handler(text='Hi')
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message_handler(text='Calories')
+async def set_gender(message):
+    await message.answer('Введите свой пол в формате "М/Ж"')
+    await UserState.gender.set()
+
+
+@dp.message_handler(state=UserState.gender)
+async def set_age(message, state):
+    await state.update_data(gender=message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой рост в сантиметрах:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой вес в килограммах:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    if data['gender'] == 'М' or data['gender'] == 'м':
+        norm = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5
+    else:
+        norm = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161
+    await message.answer(f"Ваша суточная нома {norm} калорий.")
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
